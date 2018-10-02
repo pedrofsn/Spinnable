34a715a044aed754baad6404212bf9d77307ff39
Pedro Francisco de Sousa Neto <pedrokra@gmail.com>
2018-10-02 18:48:39 -0300

    - Updating libraries - Enable kotlin experimental - Define Spinnable as Parcelable

--- spinnable/src/main/java/br/com/redcode/spinnable/library/model/Spinnable.kt
+++ spinnable/src/main/java/br/com/redcode/spinnable/library/model/Spinnable.kt
@@ -1,11 +1,14 @@
 package br.com.redcode.spinnable.library.model
 
+import android.os.Parcelable
 import br.com.redcode.spinnable.library.domain.Selectable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Spinnable(
         val id: String,
         val description: String,
         override var selected: Boolean = false
-) : Selectable {
+) : Parcelable, Selectable {
     override fun toString() = description
 }
